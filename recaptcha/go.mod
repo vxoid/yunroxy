ea8ed0369e76d65e03551f06b5d91880aab134f2
vxoid <cursed.void.dl@gmail.com>
2024-09-05 10:18:00 +0300

    add default recaptchav3 creation; todo: bypass using captcha sovling api

---
+++ recaptcha/go.mod
@@ -0,0 +1,9 @@
+module github.com/vxoid/yunroxy/recaptcha
+
+go 1.19
+
+require github.com/vxoid/yunroxy/proxy v0.0.0
+
+require golang.org/x/exp v0.0.0-20240823005443-9b4947da3948 // indirect
+
+replace github.com/vxoid/yunroxy/proxy => ../proxy
---
+++ recaptcha/v3/v3.go
@@ -0,0 +1,115 @@
+package v3
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+
+	yp "github.com/vxoid/yunroxy/proxy"
+)
+
+type ReCaptchaV3 struct {
+	site    string
+	siteKey string
+	token   string
+	proxy   *url.URL
+}
+
+const ValidateData = "!q62grYxHRvVxjUIjSFNd0mlvrZ-iCgIHAAAB6FcAAAANnAkBySdqTJGFRK7SirleWAwPVhv9-XwP8ugGSTJJgQ46-0IMBKN8HUnfPqm4sCefwxOOEURND35prc9DJYG0pbmg_jD18qC0c-lQzuPsOtUhHTtfv3--SVCcRvJWZ0V3cia65HGfUys0e1K-IZoArlxM9qZfUMXJKAFuWqZiBn-Qi8VnDqI2rRnAQcIB8Wra6xWzmFbRR2NZqF7lDPKZ0_SZBEc99_49j07ISW4X65sMHL139EARIOipdsj5js5JyM19a2TCZJtAu4XL1h0ZLfomM8KDHkcl_b0L-jW9cvAe2K2uQXKRPzruAvtjdhMdODzVWU5VawKhpmi2NCKAiCRUlJW5lToYkR_X-07AqFLY6qi4ZbJ_sSrD7fCNNYFKmLfAaxPwPmp5Dgei7KKvEQmeUEZwTQAS1p2gaBmt6SCOgId3QBfF_robIkJMcXFzj7R0G-s8rwGUSc8EQzT_DCe9SZsJyobu3Ps0-YK-W3MPWk6a69o618zPSIIQtSCor9w_oUYTLiptaBAEY03NWINhc1mmiYu2Yz5apkW_KbAp3HD3G0bhzcCIYZOGZxyJ44HdGsCJ-7ZFTcEAUST-aLbS-YN1AyuC7ClFO86CMICVDg6aIDyCJyIcaJXiN-bN5xQD_NixaXatJy9Mx1XEnU4Q7E_KISDJfKUhDktK5LMqBJa-x1EIOcY99E-eyry7crf3-Hax3Uj-e-euzRwLxn2VB1Uki8nqJQVYUgcjlVXQhj1X7tx4jzUb0yB1TPU9uMBtZLRvMCRKvFdnn77HgYs5bwOo2mRECiFButgigKXaaJup6NM4KRUevhaDtnD6aJ8ZWQZTXz_OJ74a_OvPK9eD1_5pTG2tUyYNSyz-alhvHdMt5_MAdI3op4ZmcvBQBV9VC2JLjphDuTW8eW_nuK9hN17zin6vjEL8YIm_MekB_dIUK3T1Nbyqmyzigy-Lg8tRL6jSinzdwOTc9hS5SCsPjMeiblc65aJC8AKmA5i80f-6Eg4BT305UeXKI3QwhI3ZJyyQAJTata41FoOXl3EF9Pyy8diYFK2G-CS8lxEpV7jcRYduz4tEPeCpBxU4O_KtM2iv4STkwO4Z_-c-fMLlYu9H7jiFnk6Yh8XlPE__3q0FHIBFf15zVSZ3qroshYiHBMxM5BVQBOExbjoEdYKx4-m9c23K3suA2sCkxHytptG-6yhHJR3EyWwSRTY7OpX_yvhbFri0vgchw7U6ujyoXeCXS9N4oOoGYpS5OyFyRPLxJH7yjXOG2Play5HJ91LL6J6qg1iY8MIq9XQtiVZHadVpZVlz3iKcX4vXcQ3rv_qQwhntObGXPAGJWEel5OiJ1App7mWy961q3mPg9aDEp9VLKU5yDDw1xf6tOFMwg2Q-PNDaKXAyP_FOkxOjnu8dPhuKGut6cJr449BKDwbnA9BOomcVSztEzHGU6HPXXyNdZbfA6D12f5lWxX2B_pobw3a1gFLnO6mWaNRuK1zfzZcfGTYMATf6d7sj9RcKNS230XPHWGaMlLmNxsgXkEN7a9PwsSVwcKdHg_HU4vYdRX6vkEauOIwVPs4dS7yZXmtvbDaX1zOU4ZYWg0T42sT3nIIl9M2EeFS5Rqms_YzNp8J-YtRz1h5RhtTTNcA5jX4N-xDEVx-vD36bZVzfoMSL2k85PKv7pQGLH-0a3DsR0pePCTBWNORK0g_RZCU_H898-nT1syGzNKWGoPCstWPRvpL9cnHRPM1ZKemRn0nPVm9Bgo0ksuUijgXc5yyrf5K49UU2J5JgFYpSp7aMGOUb1ibrj2sr-D63d61DtzFJ2mwrLm_KHBiN_ECpVhDsRvHe5iOx_APHtImevOUxghtkj-8RJruPgkTVaML2MEDOdL_UYaldeo-5ckZo3VHss7IpLArGOMTEd0bSH8tA8CL8RLQQeSokOMZ79Haxj8yE0EAVZ-k9-O72mmu5I0wH5IPgapNvExeX6O1l3mC4MqLhKPdOZOnTiEBlSrV4ZDH_9fhLUahe5ocZXvXqrud9QGNeTpZsSPeIYubeOC0sOsuqk10sWB7NP-lhifWeDob-IK1JWcgFTytVc99RkZTjUcdG9t8prPlKAagZIsDr1TiX3dy8sXKZ7d9EXQF5P_rHJ8xvmUtCWqbc3V5jL-qe8ANypwHsuva75Q6dtqoBR8vCE5xWgfwB0GzR3Xi_l7KDTsYAQIrDZVyY1UxdzWBwJCrvDrtrNsnt0S7BhBJ4ATCrW5VFPqXyXRiLxHCIv9zgo-NdBZQ4hEXXxMtbem3KgYUB1Rals1bbi8X8MsmselnHfY5LdOseyXWIR2QcrANSAypQUAhwVpsModw7HMdXgV9Uc-HwCMWafOChhBr88tOowqVHttPtwYorYrzriXNRt9LkigESMy1bEDx79CJguitwjQ9IyIEu8quEQb_-7AEXrfDzl_FKgASnnZLrAfZMtgyyddIhBpgAvgR_c8a8Nuro-RGV0aNuunVg8NjL8binz9kgmZvOS38QaP5anf2vgzJ9wC0ZKDg2Ad77dPjBCiCRtVe_dqm7FDA_cS97DkAwVfFawgce1wfWqsrjZvu4k6x3PAUH1UNzQUxVgOGUbqJsaFs3GZIMiI8O6-tZktz8i8oqpr0RjkfUhw_I2szHF3LM20_bFwhtINwg0rZxRTrg4il-_q7jDnVOTqQ7fdgHgiJHZw_OOB7JWoRW6ZlJmx3La8oV93fl1wMGNrpojSR0b6pc8SThsKCUgoY6zajWWa3CesX1ZLUtE7Pfk9eDey3stIWf2acKolZ9fU-gspeACUCN20EhGT-HvBtNBGr_xWk1zVJBgNG29olXCpF26eXNKNCCovsILNDgH06vulDUG_vR5RrGe5LsXksIoTMYsCUitLz4HEehUOd9mWCmLCl00eGRCkwr9EB557lyr7mBK2KPgJkXhNmmPSbDy6hPaQ057zfAd5s_43UBCMtI-aAs5NN4TXHd6IlLwynwc1zsYOQ6z_HARlcMpCV9ac-8eOKsaepgjOAX4YHfg3NekrxA2ynrvwk9U-gCtpxMJ4f1cVx3jExNlIX5LxE46FYIhQ"
+
+func New(site string, siteKey string, proxy *url.URL) (*ReCaptchaV3, error) {
+	if proxy != nil && !yp.IsSsl(proxy) {
+		return nil, yp.ErrProxyMustBeSSL
+	}
+
+	recaptcha := ReCaptchaV3{site: site, siteKey: siteKey, proxy: proxy}
+	client := recaptcha.getHttpClient()
+
+	siteEncoded := base64.StdEncoding.EncodeToString([]byte(site))
+	url := fmt.Sprintf("https://www.google.com/recaptcha/api2/anchor?ar=1&k=%s&co=%s&hl=en&type=image&theme=light&size=invisible&badge=bottomright", siteKey, siteEncoded)
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	re := regexp.MustCompile(`<input[^>]*id="recaptcha-token"[^>]*value="([^"]*)"`)
+
+	match := re.FindStringSubmatch(string(respBody))
+	if match == nil || len(match) < 1 {
+		return nil, fmt.Errorf("no token was found in anchor response (it must be wrong site or siteKey)")
+	}
+
+	recaptcha.token = match[1]
+	err = recaptcha.Reload(ValidateData)
+	if err != nil {
+		return nil, err
+	}
+
+	return &recaptcha, nil
+}
+
+func Bypass(site string, siteKey string, proxy *url.URL) (*ReCaptchaV3, error) {
+	return nil, fmt.Errorf("unimplemented")
+}
+
+func (r *ReCaptchaV3) getHttpClient() http.Client {
+	client := http.Client{}
+	if r.proxy != nil {
+		client.Transport = &http.Transport{Proxy: http.ProxyURL(r.proxy)}
+	}
+
+	return client
+}
+
+func (r *ReCaptchaV3) Reload(data string) error {
+	client := r.getHttpClient()
+
+	siteEncoded := base64.StdEncoding.EncodeToString([]byte(r.site))
+	body := fmt.Sprintf("reason=q&c=%s&k=%s&co=%s&hl=en&size=invisible&bq=%s", r.token, r.siteKey, siteEncoded, data)
+	request, err := http.NewRequest(http.MethodPost, "https://www.google.com/recaptcha/api2/reload", bytes.NewReader([]byte(body)))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("%s: %s", resp.Status, string(respBody))
+	}
+
+	var parsedResp []interface{}
+	err = json.Unmarshal([]byte(strings.Split(string(respBody), "\n")[1]), &parsedResp)
+	if err != nil {
+		return err
+	}
+
+	r.token = parsedResp[1].(string)
+
+	return nil
+}
+
+func (r *ReCaptchaV3) GetToken() string {
+	return r.token
+}
