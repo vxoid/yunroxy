edb005b328a6f45d129348e6796f74a2cd1b273a
vxoid <cursed.void.dl@gmail.com>
2024-09-05 11:04:13 +0300

    add parse

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
-	"strconv"
 
 	"golang.org/x/exp/slices"
 )
@@ -21,9 +20,18 @@ func GetSupportedProtocols() []string {
 	return []string{"http", "https", "socks4", "socks5"}
 }
 
-func NewProxy(proto string, hostname string, port uint16, user *url.Userinfo) (*url.URL, error) {
+func Parse(addr string) (*url.URL, error) {
+	link, err := url.Parse(addr)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewProxy(link.Scheme, link.Host, link.User)
+}
+
+func NewProxy(proto string, host string, user *url.Userinfo) (*url.URL, error) {
 	if !slices.Contains(GetSupportedProtocols(), proto) {
 		return nil, fmt.Errorf("\"%s\" protocol is not supported", proto)
 	}
-	return &url.URL{Scheme: proto, User: user, Host: hostname + ":" + strconv.FormatUint(uint64(port), 10)}, nil
+	return &url.URL{Scheme: proto, User: user, Host: host}, nil
 }
