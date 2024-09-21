f9d308b827d6c7b439f6ce3f90b3ba6f2ccaac92
vxoid <cursed.void.dl@gmail.com>
2024-09-21 23:19:21 +0300

    edit db api, renamed funcs

--- db/db.go
+++ db/db.go
@@ -1,79 +1,110 @@
 package db
 
 import (
+	"encoding/hex"
 	"fmt"
 	"math/rand"
+	"net/url"
 
+	"github.com/vxoid/yunroxy/proxy"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-type ApiDb struct {
+type YunroxyDb struct {
 	Db *gorm.DB
 }
 
-func NewApiDb(dbPath string) (*ApiDb, error) {
+func NewApiDb(dbPath string) (*YunroxyDb, error) {
 	var db, Err = gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
 	if Err != nil {
 		return nil, Err
 	}
 	db.AutoMigrate(&User{}, &Proxy{})
-	return &ApiDb{Db: db}, nil
+	return &YunroxyDb{Db: db}, nil
 }
 
-func (slf ApiDb) IsApiKey(key string) bool {
-	var user User
-	return slf.Db.First(&user, "api_key = ?", key).Error == nil
-}
-
-func (slf ApiDb) AddProxy(serviceUrl string, proxyUrl string) {
-	slf.Db.Create(&Proxy{Service: serviceUrl, ProxyUrl: proxyUrl})
-}
-
-func (slf ApiDb) DelProxy(proxyUrl string) {
-	var proxy Proxy
-	res := slf.Db.Take(&proxy, "proxy_url = ?", proxyUrl)
-	if res.Error != nil {
-		return
+func (slf YunroxyDb) GetUserByApiKey(apiKeyHex string) (*User, error) {
+	apiKey, err := hex.DecodeString(apiKeyHex)
+	if err != nil {
+		return nil, err
 	}
-	slf.Db.Unscoped().Delete(Proxy{ProxyUrl: proxyUrl})
+
+	var users []User
+	result := slf.Db.Limit(1).Find(&users, "api_key = ?", apiKey)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	if len(users) < 1 {
+		return nil, fmt.Errorf("'%s' is not valid api key", apiKey)
+	}
+
+	return &users[0], nil
 }
 
-func (slf ApiDb) GetProxy(apikey string) (string, error) {
-	if !slf.IsApiKey(apikey) {
-		return "", fmt.Errorf("invalid api key")
+func (slf YunroxyDb) AddProxy(serviceUrl string, proxyUrl *url.URL) {
+	slf.Db.Create(&Proxy{Service: serviceUrl, ProxyUrl: proxyUrl.String()})
+}
+
+func (slf YunroxyDb) DeleteProxy(proxyUrl *url.URL) error {
+	return slf.deleteProxy(proxyUrl.String())
+}
+
+func (slf YunroxyDb) deleteProxy(proxyUrl string) error {
+	return slf.Db.Where("proxy_url = ?", proxyUrl).Delete(&Proxy{}).Error
+}
+
+func (slf YunroxyDb) GetRandomProxy(apiKeyHex string) (*url.URL, error) {
+	_, err := slf.GetUserByApiKey(apiKeyHex)
+	if err != nil {
+		return nil, err
 	}
-	var proxy, last Proxy
+
+	var proxyAssoc, last Proxy
 	max := slf.Db.Last(&last)
 	if max.Error != nil {
-		return "", max.Error
+		return nil, max.Error
 	}
-	res := slf.Db.Find(&proxy, randRange(1, int(last.ID)))
+
+	res := slf.Db.Find(&proxyAssoc, randRange(1, int(last.ID)))
 	if res.Error != nil {
-		return "", res.Error
+		return nil, res.Error
 	}
-	return proxy.ProxyUrl, nil
+	return proxy.Parse(proxyAssoc.ProxyUrl)
 }
 
-func (slf ApiDb) GetAllProxies() []string {
-	var proxies []string
-	var proxy Proxy
-	var len = slf.Db.Find(&proxy).RowsAffected
-	for i := 0; i < int(len); i++ {
-		proxies = append(proxies, proxy.ProxyUrl)
+func (slf YunroxyDb) GetAllProxies() ([]*url.URL, error) {
+	var proxies []Proxy
+	res := slf.Db.Find(&proxies)
+	if res.Error != nil {
+		return nil, res.Error
 	}
-	return proxies
+
+	var result []*url.URL
+	for _, proxyAssoc := range proxies {
+		proxyUrl, err := proxy.Parse(proxyAssoc.ProxyUrl)
+		if err != nil {
+			slf.deleteProxy(proxyAssoc.ProxyUrl)
+			continue
+		}
+		result = append(result, proxyUrl)
+	}
+	return result, nil
 }
 
 func randRange(min, max int) int {
 	return rand.Intn(max-min+1) + min
 }
 
-func (slf ApiDb) CreateApiKey() ([]byte, error) {
+func (slf YunroxyDb) CreateApiKey() ([]byte, error) {
 	bytes := make([]byte, 256)
 	if _, err := rand.Read(bytes); err != nil {
 		return nil, err
 	}
-	slf.Db.Create(&User{ApiKey: bytes})
+
+	res := slf.Db.Create(&User{ApiKey: bytes})
+	if res.Error != nil {
+		return nil, res.Error
+	}
 	return bytes, nil
 }
--- db/go.mod
+++ db/go.mod
@@ -7,9 +7,14 @@ require (
 	gorm.io/gorm v1.25.11
 )
 
+require golang.org/x/exp v0.0.0-20240823005443-9b4947da3948 // indirect
+
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.23 // indirect
+	github.com/vxoid/yunroxy/proxy v0.0.0
 	golang.org/x/text v0.18.0 // indirect
 )
+
+replace github.com/vxoid/yunroxy/proxy => ../proxy
